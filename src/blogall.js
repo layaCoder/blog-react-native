2965eb8b9a34208481dcf13b5dd2020b9e65b2b0
laya <412046213@qq.com>
2020-10-09 09:42:38 +0800

    update-load-next-page

--- src/blogall.js
+++ src/blogall.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Text, View, Image } from 'react-native';
+import { StyleSheet, FlatList, Text, View, Image } from 'react-native';
 import config from '../config'
 import apis from '../apis/index'
 import BlogItem from './BlogItem'
@@ -9,7 +9,8 @@ export default class BlogAll extends React.Component {
         super(props);
         this.state = {
             isLoading: true,
-            dataSource: []
+            dataSource: [],
+            page: 1
         }
     }
 
@@ -28,17 +29,58 @@ export default class BlogAll extends React.Component {
             });
     }
 
+    _keyExtractor = (item) => {
+        return item._id
+    }
+
+    _renderItem = ({ item }) => {
+        return <BlogItem item={item} {...this.props}></BlogItem>
+    }
+
+    onEndReached = () => {
+        if (!this.onEndReachedCalledDuringMomentum) {
+            this.loadNextPage() // fetch data
+            this.onEndReachedCalledDuringMomentum = true;
+        }
+    };
+
+    loadNextPage = () => {
+        this.setState({ isLoading: true })
+        fetch(`${config.server}${apis.blogList.devUrl}?pageIndex=${this.state.page + 1}&pageSize=10`)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log('blog item ...>', responseJson)
+                let res = this.state.dataSource.concat(responseJson)
+                this.setState(prevState => ({ dataSource: res, isLoading: false, page: this.state.page + 1 }))
+            })
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, paddingTop: 20 }}>
+            <View style={{ flex: 1, paddingTop: 10 }}>
+                {this.state.isLoading ? <Text>Loading ...</Text> : null}
                 <FlatList
                     data={this.state.dataSource}
-                    renderItem={({ item }) =>
-
-                        <BlogItem item={item} {...this.props}></BlogItem>
-                    }
+                    keyExtractor={this._keyExtractor}
+                    // renderItem={({ item }) =>
+                    //     <BlogItem item={item} {...this.props}></BlogItem>
+                    // }
+                    renderItem={this._renderItem}
+                    onEndReached={this.onEndReached}
+                    onEndReachedThreshold={0.5}
+                    onMomentumScrollBegin={() => {
+                        this.onEndReachedCalledDuringMomentum = false;
+                    }}
                 ></FlatList>
             </View >
         );
     }
 }
+
+const styles = StyleSheet.create({
+    Loading: {
+        color: 'red',
+        textAlign: 'center'
+    }
+
+});
